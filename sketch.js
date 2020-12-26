8cc8488285ba4285758404df21f3f597f4544c43
joshuamv <joshuajmv@me.com>
2020-12-26 16:50:05 +0200

    soul

--- sketch.js
+++ sketch.js
@@ -1,39 +1,68 @@
-/////// p5 sketch ///////
+//globl vars//
 
-var s = function( p ) { // p could be any variable name
-  var circleSize = 0;
-  p.setup = function() {
-    p.createCanvas(330, 350);
-  };
+var sf = 1; // scaleFactor
+var mx, my; // mouse coordinates;
 
-  p.draw = function() {
-    p.background(0, 5);
-    //circular lines
-    p.fill(0, 4);
-    p.strokeWeight(2);
-    p.stroke(255);
-    p.translate(340/2, 350/2);
-    p.ellipse(0, 0, 265);
-    p.ellipse(0, 0, 205);
-    p.ellipse(0, 0, 145);
-    p.ellipse(0, 0, 85);
-    p.ellipse(0, 0, 25);
-    //moving filling
-    p.push();
-    p.rotate((-1 * p.PI) + (1 * p.PI) * (p.frameCount % 1000) / 250);
-    p.strokeWeight(2);
-    p.stroke(255, 190);
-    p.line(0, 0, 265/2, 0);
-    p.pop();
-    //circle pulse
-    p.fill(0, 0);
-    p.strokeWeight(5);
-    p.stroke(255, 15);
-    circleSize += 2.3;
-    if (circleSize > 265){
-       circleSize = 0
-    }
-    p.ellipse(0, 0, circleSize);
-  };
-};
-var myp5 = new p5(s, 'p5-radar');
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noCursor();
+}
+
+function draw() {
+  mx = mouseX;
+  my = mouseY;
+
+  background(0);
+  //scale and keep shapes centered
+  translate(mx, my);
+  scale(sf);
+  translate(-mx, -my);
+
+
+  //draw cricle and neurons
+  soulCircle(12,8);
+  neurons();
+
+
+  if (mouseIsPressed) {
+    soulCircle(25,20);
+    neuronsFire();
+  }
+
+}
+
+window.addEventListener("wheel", function(e) {
+  if (e.deltaY > 0)
+    sf *= 1.05;
+  else
+    sf *= 0.91;
+});
+
+
+///functions///
+
+function soulCircle(szBig,szSmall){
+  background(0, 0, 20);
+  noStroke();
+  //moving shadows
+  fill(150, 220, 255, 30);
+  ellipse((mouseX+(sin(frameCount*0.05)+1)*1.01), (mouseY-(sin(frameCount*0.06)+1)*1.08), (sin(frameCount*0.02)+19)*2.1);
+  ellipse((mouseX-(sin(frameCount*0.06)+1)*1.01), (mouseY+(sin(frameCount*0.08)+1)*1.03), (sin(frameCount*0.02)+19)*2.1);
+  ellipse((mouseX-(sin(frameCount*0.08)+1)*1.02), (mouseY+(sin(frameCount*0.05)+1)*1.01), (sin(frameCount*0.02)+19)*2.1);
+  //big ellipse
+  fill(255, 255, 255, 230);
+  ellipse(mouseX, mouseY, (sin(frameCount*0.02)+szBig));
+  //little ellipse pointer
+  fill(255);
+  ellipse(mouseX, mouseY, (sin(frameCount*0.04)+szSmall));
+}
+
+function neurons(){
+  fill(255, 255, 0);
+  ellipse(mouseX, mouseY, 6)
+}
+
+function neuronsFire(){
+  fill(0, 50, 255);
+  ellipse(mouseX, mouseY, 16)
+}
